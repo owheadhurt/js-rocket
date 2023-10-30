9d2207ba95f8b273a492b5fd55b75fbdbe6a54fc
Ryan Crosby <Ryansjobbox@gmail.com>
2023-10-30 00:14:54 -0400

    made car changes

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var car = {
+			make: 'VW',
+			type: 'Polo',
+			color: 'blue',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: ['seat 1', 
+					'seat 2', 
+					'seat 3', 
+					'seat 4'],
+			turnOn: function () {
+				this.isTurnedOn = true;
+			},
+			fly: function () {
+				alert('fly');
+			},
+			switchCar: function (isOn) {
+				console.log('turn car' +isOn)
+				;
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				}	else {
+						this.isTurnedOn = false;
+					}
+				}
+			};
+//this is turning the car on and off based on true and false statements which can by attributed to buttons and objects.
+		console.log('hello there friends!');
